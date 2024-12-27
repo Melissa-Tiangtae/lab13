8ce175d972e1b484de8235ee09d0c3cdc8a11d70
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-27 16:08:56 +0700

    lab13_2_completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -13,7 +13,7 @@ int main()
 {
     bool image[N][M] = {};
     int s,x,y;
-    do{
+   do{
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
@@ -21,3 +21,40 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+
+  for(int i=-1;i<N+1;i++){
+    for(int j=-1; j<M+1; j++){
+
+        if(i== -1 || i==N) cout << "-";
+        else if(j==-1 || j== M) cout << "|";
+        else if(image [i][j]) cout << "*";
+        else cout << " ";
+
+    }
+
+
+    cout << endl;
+  }
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+
+  
+
+    for(int i=0;i<N;i++){
+        for(int j=0; j<M; j++){
+              bool condition = sqrt(pow((i-x),2) + pow((j-y),2)) <= (s-1);
+            if(condition){
+                image[i][j] = true;
+            }
+
+        }
+
+
+    }
+     
+
+}
+
